592782f921cf3b4d10f0a3acefb3a4d22119f17b
Daniel Vicente <danielvicente63@gmail.com>
2025-09-04 22:21:55 -0300

    Ajustes pensados em mobile.

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPencil } from "@fortawesome/free-solid-svg-icons";
+import TeamCounter from "./components/TeamCounter";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  function handleClickAdd() {
-    setCount((count) => count + 1);
-  }
-
-  function handleClickSubtract() {
-    setCount((count) => count - 1);
-  }
-
   return (
-    <>
-      <div className="flex justify-center items-center h-full w-full bg-indigo-200">
-        <div className="w-1/2 bg-gray-400 h-full flex flex-col justify-center items-center gap-4 p-5 rounded-md">
-          <p>
-            Time 1 <FontAwesomeIcon icon={faPencil} />
-          </p>
-          <p>{count}</p>
-          <button
-            className="w-[calc(100%-20px)]  bg-green-400 rounded-md"
-            onClick={() => handleClickAdd()}
-          >
-            +1
-          </button>
-          <button
-            className="w-[calc(100%-20px)]  bg-red-400 rounded-md"
-            onClick={() => handleClickSubtract()}
-          >
-            -1
-          </button>
-        </div>
+    <div className="flex justify-center items-center h-screen w-full bg-indigo-200 flex-col relative">
+      <div className="w-full flex h-full">
+        <TeamCounter initialName="Time 1" />
+        <TeamCounter initialName="Time 2" />
       </div>
-    </>
+      <div className="absolute bottom-0">TRUCO</div>
+    </div>
   );
 }
 
---
+++ src/components/TeamCounter.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+function TeamCounter({ initialName = "Time" }) {
+  const [count, setCount] = useState(0);
+  const [teamName, setTeamName] = useState(initialName);
+
+  return (
+    <div className="w-1/2 bg-gray-400 h-full flex flex-col justify-center items-center gap-4 p-5 rounded-md">
+      <input
+        className="flex gap-5 items-center focus:outline-none text-3xl font-bebas text-center"
+        onChange={(e) => setTeamName(e.target.value)}
+        value={teamName}
+      />
+      <p className="text-3xl font-bebas">{count}</p>
+
+      <button
+        className="w-[calc(100%-20px)] bg-green-400 rounded-md"
+        onClick={() => setCount((c) => c + 1)}
+      >
+        +1
+      </button>
+      <button
+        className="w-[calc(100%-20px)] bg-red-400 rounded-md"
+        onClick={() => setCount((c) => c - 1)}
+      >
+        -1
+      </button>
+    </div>
+  );
+}
+
+export default TeamCounter;
