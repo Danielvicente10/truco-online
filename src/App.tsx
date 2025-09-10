aba71de8809ba94cc42fe028d0bc3808bf3e3935
Daniel Vicente <danielvicente63@gmail.com>
2025-09-09 23:03:40 -0300

    Ajustes para marcação de ponto

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
+import { useState } from "react";
 import "./App.css";
 import TeamCounter from "./components/TeamCounter";
 
 function App() {
+  const [trucoValue, setTrucoValue] = useState(3); // começa no 3
+
+  function handleClickTruco() {
+    setTrucoValue((prev) => {
+      if (prev >= 12) return 3; // reinicia no 3
+      return prev + 3;
+    });
+  }
+
   return (
-    <div className="flex justify-center items-center h-screen w-full bg-indigo-200 flex-col relative">
+    <div className="flex justify-center items-center h-full w-full bg-indigo-200 flex-col relative">
       <div className="w-full flex h-full">
-        <TeamCounter initialName="Time 1" />
-        <TeamCounter initialName="Time 2" />
+        <TeamCounter initialName="Time 1" trucoValue={trucoValue} />
+        <TeamCounter initialName="Time 2" trucoValue={trucoValue} />
       </div>
-      <div className="absolute bottom-0">TRUCO</div>
+      <div
+        className="absolute bottom-24 text-6xl bg-cyan-500 rounded-md w-72 flex justify-center items-center p-4 text-black font-bold cursor-pointer select-none"
+        onClick={handleClickTruco}
+      >
+        TRUCO ({trucoValue})
+      </div>
     </div>
   );
 }
--- src/components/TeamCounter.tsx
+++ src/components/TeamCounter.tsx
@@ -1,8 +1,18 @@
 import { useState } from "react";
 
-function TeamCounter({ initialName = "Time" }) {
+function TeamCounter({
+  initialName = "Time",
+  trucoValue,
+}: {
+  initialName?: string;
+  trucoValue: number;
+}) {
   const [count, setCount] = useState(0);
   const [teamName, setTeamName] = useState(initialName);
+
+  function handleClickTruco() {
+    setCount((c) => c + trucoValue);
+  }
 
   return (
     <div className="w-1/2 bg-gray-400 h-full flex flex-col justify-center items-center gap-4 p-5 rounded-md">
@@ -24,6 +34,12 @@ function TeamCounter({ initialName = "Time" }) {
         onClick={() => setCount((c) => c - 1)}
       >
         -1
+      </button>
+      <button
+        className="w-[calc(100%-20px)] bg-yellow-400 rounded-md"
+        onClick={handleClickTruco}
+      >
+        TRUCO +{trucoValue}
       </button>
     </div>
   );
